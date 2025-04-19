775f111909fdc88a823ed8c429c909f8134368ee
MOHANASUNDAR R <166241992+Mohanasundar2004@users.noreply.github.com>
2025-04-19 19:46:50 +0530

    Time: 0 ms (100%), Space: 40.6 MB (73%) - LeetHub

---
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int findTheWinner(int n, int k) {
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum=(sum+k)%i;
+        }
+        return sum+1;
+    }
+}
